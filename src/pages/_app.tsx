131e94d1d24eb3fd58ce03054a72c1bfd5e51b54
vincentbollaert <vincentbollaert@gmail.com>
2022-06-18 22:12:37 +0200

    remove unused styles and styling libs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,7 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
